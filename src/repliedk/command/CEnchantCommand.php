67a3d37a3b2794c40f304dcea152fbfe49bd8f85
RepliedK <repliedk@gmail.com>
2024-02-18 13:35:31 -0600

    update command

--- src/repliedk/command/CEnchantCommand.php
+++ src/repliedk/command/CEnchantCommand.php
@@ -18,39 +18,39 @@ class CEnchantCommand extends VanillaCommand
         if (!$sender instanceof Player)
             return;
         if (!$sender->hasPermission(DefaultPermissions::ROOT_OPERATOR)) {
-            $sender->sendMessage(TextFormat::colorize('&cYou do not have permission to use this command'));
+            $sender->sendMessage(TextFormat::colorize("&cYou do not have permission to use this command"));
             return;
         }    
         if (count($args) < 1) {
-            $sender->sendMessage(TextFormat::colorize('&c/cenchant [string: enchantName] or /cenchant list'));
+            $sender->sendMessage(TextFormat::colorize("&c/cenchant [string: enchantName] or /cenchant list"));
             return;
         }
-        if ($args[0] === 'list') {
-            $sender->sendMessage(TextFormat::colorize('&aEnchantments list: '.implode(':', CustomEnchants::getInstance()->customEnchants)));
+        if ($args[0] === "list") {
+            $sender->sendMessage(TextFormat::colorize("&aEnchantments list: ".implode(":", CustomEnchants::getInstance()->customEnchants)));
         }
         if (!is_string($args[0])) {
-            $sender->sendMessage(TextFormat::colorize('&cInvalid Name'));
+            $sender->sendMessage(TextFormat::colorize("&cInvalid Name"));
             return;
         }
         if (($enchantment = CustomEnchants::getInstance()->getEnchant($args[0])) === null) {
-            $sender->sendMessage(TextFormat::colorize('&cThis enchantment does not exist'));
+            $sender->sendMessage(TextFormat::colorize("&cThis enchantment does not exist"));
             return;
         }
         $item = clone $sender->getInventory()->getItemInHand();
         if ($item->isNull()) {
-            $sender->sendMessage(TextFormat::colorize('&cThis item cannot be enchanted'));
+            $sender->sendMessage(TextFormat::colorize("&cThis item is null"));
             return;
         }
         if ($item->hasEnchantment($enchantment)) {
-            $sender->sendMessage(TextFormat::colorize('&cThe item already contains this enchantment'));
+            $sender->sendMessage(TextFormat::colorize("&cThe item already contains this enchantment"));
             return;
         }
         $lore = $item->getLore();
-        $lore[] = TextFormat::colorize('&6' . $enchantment->getName());
+        $lore[] = TextFormat::colorize("&6" . $enchantment->getName());
         $item->setLore($lore);
         $item->addEnchantment(new EnchantmentInstance($enchantment, $enchantment->getMaxLevel()));
         $sender->getInventory()->setItemInHand($item);
-        $sender->sendMessage(TextFormat::colorize('&aYou have successfully enchanted the item'));
+        $sender->sendMessage(TextFormat::colorize("&aYou have successfully enchanted the item"));
     }
 
 }
